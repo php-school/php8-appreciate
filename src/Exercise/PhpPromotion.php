af68272377ea9baceab164ca8992caf5fbec94f1
Michael Woodward <mikeymike.mw@gmail.com>
2020-12-17 22:14:23 +0000

    Property Promotion checks refactor, first pass

--- src/Exercise/PhpPromotion.php
+++ src/Exercise/PhpPromotion.php
@@ -63,15 +63,116 @@ class PhpPromotion extends AbstractExercise implements ExerciseInterface, Provid
             return Failure::fromNameAndReason($this->getName(), 'No code was found');
         }
 
+        $className = $this->getClassName($statements);
+
+        if (!$className) {
+            return Failure::fromNameAndReason($this->getName(), 'No class was found');
+        }
+
+        (static fn () => require $input->getRequiredArgument('program'))();
+
+        $reflectionClass = new ReflectionClass($className);
+        $params = collect($reflectionClass->getMethod('__construct')->getParameters());
+        $params = $params->flatMap(fn (\ReflectionParameter $prop) => [$prop->getName() => $prop]);
+
+        if ($missing = array_diff(['visitor', 'key', 'config'], $params->keys()->getArrayCopy())) {
+            $plural = count($missing) > 1;
+            return Failure::fromNameAndReason($this->getName(), sprintf(
+                'Propert%s "%s" %s missing',
+                $plural ? 'ies' : 'y',
+                implode('" & "', $missing),
+                $plural ? 'were' : 'was'
+            ));
+        }
+
+        $promoted = $params->filter(fn (\ReflectionParameter $prop) => $prop->isPromoted());
+
+        if ($notPromoted = array_diff(['visitor', 'key'], $promoted->keys()->getArrayCopy())) {
+            $plural = count($notPromoted) > 1;
+            return Failure::fromNameAndReason($this->getName(), sprintf(
+                'Propert%s "%s" %s not promoted',
+                $plural ? 'ies' : 'y',
+                implode('" & "', $notPromoted),
+                $plural ? 'were' : 'was'
+            ));
+        }
+
+        if ($promoted->get('config')) {
+            return Failure::fromNameAndReason($this->getName(), 'Property "config" should not be promoted');
+        }
+
+        $properties = collect($reflectionClass->getProperties());
+        $properties = $properties->flatMap(fn (\ReflectionProperty $prop) => [$prop->getName() => $prop]);
+        $private = $properties->filter(fn (\ReflectionProperty $prop) => $prop->isPrivate());
+
+        // TODO: New test case, basePath changing visibility?!
+        if ($notPrivate = array_diff(['visitor', 'key'], $private->keys()->getArrayCopy())) {
+            $plural = count($notPrivate) > 1;
+            return Failure::fromNameAndReason($this->getName(), sprintf(
+                'Propert%s "%s" visibility changed',
+                $plural ? 'ies' : 'y',
+                implode('" & "', $notPrivate)
+            ));
+        }
+
+        $types = $properties->map(fn (\ReflectionProperty $prop) => $prop->getType()->getName());
+        $expected = ['visitor' => 'Closure', 'key' => 'string', 'basePath' => 'string'];
+        if ([] !== $typeDiff = array_diff_assoc($expected, $types->getArrayCopy())) {
+            $plural = count($typeDiff) > 1;
+            return Failure::fromNameAndReason($this->getName(), sprintf(
+                'Propert%s "%s" should not have changed type',
+                $plural ? 'ies' : 'y',
+                implode('" & "', array_keys($typeDiff))
+            ));
+        }
+
+        // Test Invocation
+        $args = [
+            'visitor' => \Closure::fromCallable(fn (array $line) => $line[2]),
+            'key' => 'value',
+            'config' => ['basePath' => '/some/base/path']
+        ];
+
+        $obj = new $className(...$args);
+
+        $actual = $properties
+            ->each(fn (\ReflectionProperty $prop) => $prop->setAccessible(true))
+            ->map(fn (\ReflectionProperty $prop) => $prop->isInitialized($obj) ? $prop->getValue($obj) : null)
+            ->getArrayCopy();
+
+        $dataFailures = array_filter([
+            'visitor' => $args['visitor'] !== $actual['visitor'],
+            'key' => $args['key'] !== $actual['key'],
+            'basePath' => $args['config']['basePath'] !== $actual['basePath'],
+        ]);
+
+        if (count($dataFailures)) {
+            $plural = count($dataFailures) > 1;
+            return Failure::fromNameAndReason($this->getName(), sprintf(
+                'Data not correctly set on propert%s "%s"',
+                $plural ? 'ies' : 'y',
+                implode('" & "', array_keys($dataFailures))
+            ));
+        }
+
+        return new Success($this->getName());
+    }
+
+    /**
+     * @param Node\Stmt[]
+     * @return string|null
+     */
+    private function getClassName(array $statements) :? string
+    {
         $traverser = new NodeTraverser();
         $traverser->addVisitor($visitor = new class extends NodeVisitorAbstract {
-            public bool $hasClasSNode = false;
-            public string $classname = '';
+            public bool $hasClassNode = false;
+            public ?string $classname = null;
 
             public function enterNode(Node $node)
             {
                 if ($node instanceof Node\Stmt\Class_) {
-                    $this->hasClasSNode = true;
+                    $this->hasClassNode = true;
                     $this->classname    = $node->name->name;
                 }
             }
@@ -79,132 +180,6 @@ class PhpPromotion extends AbstractExercise implements ExerciseInterface, Provid
 
         $traverser->traverse($statements);
 
-        if (!$visitor->hasClasSNode) {
-            return Failure::fromNameAndReason($this->getName(), 'No class was found');
-        }
-
-        return (function () use ($input, $visitor): ResultInterface {
-            require $input->getRequiredArgument('program');
-
-            $class     = new ReflectionClass($visitor->classname);
-            $construct = $class->getMethod('__construct');
-            $params    = $construct->getParameters();
-
-            $promotedProperties = [];
-            $expected = ['key', 'visitor'];
-            $failed   = [];
-            $missing  = ['key', 'visitor'];
-            $visibility = [];
-            $types = ['visitor' => 'Closure', 'key' => 'string', 'basePath' => 'string'];
-            $typeErrors = [];
-
-            foreach ($params as $param) {
-                if ($param->isPromoted()) {
-                    $promotedProperties[] = $param->getName();
-                }
-
-                if (($key = array_search($param->getName(), $missing, true)) !== false) {
-                    unset($missing[$key]);
-                }
-
-                if (!in_array($param->getName(), $expected)) {
-                    continue;
-                }
-
-                if (!$param->isPromoted()) {
-                    $failed[] = $param->getName();
-                }
-
-                if (!$class->getProperty($param->getName())->isPrivate()) {
-                    $visibility[] = $param->getName();
-                }
-
-                $expectedType = $types[$param->getName()];
-                $propertyType = $class->getProperty($param->getName())->getType();
-                $validProperty = $propertyType && $propertyType instanceof \ReflectionNamedType;
-                if ($validProperty && $expectedType !== $propertyType->getName()) {
-                    $typeErrors[] = $param->getName();
-                }
-            }
-
-            if (count($missing) > 0) {
-                $plural = count($missing) > 1;
-                return Failure::fromNameAndReason($this->getName(), sprintf(
-                    'Propert%s "%s" %s missing',
-                    $plural ? 'ies' : 'y',
-                    implode('" & "', $missing),
-                    $plural ? 'were' : 'was'
-                ));
-            }
-
-            if (count($failed) > 0) {
-                $plural = count($failed) > 1;
-                return Failure::fromNameAndReason($this->getName(), sprintf(
-                    'Propert%s "%s" %s not promoted',
-                    $plural ? 'ies' : 'y',
-                    implode('" & "', $failed),
-                    $plural ? 'were' : 'was'
-                ));
-            }
-
-            if (count($visibility) > 0) {
-                $plural = count($visibility) > 1;
-                return Failure::fromNameAndReason($this->getName(), sprintf(
-                    'Propert%s "%s" visibility changed',
-                    $plural ? 'ies' : 'y',
-                    implode('" & "', $visibility)
-                ));
-            }
-
-            if (in_array('config', $promotedProperties, true)) {
-                return Failure::fromNameAndReason($this->getName(), 'Property "config" should not be promoted');
-            }
-
-            if (count($typeErrors) > 0) {
-                $plural = count($typeErrors) > 1;
-                return Failure::fromNameAndReason($this->getName(), sprintf(
-                    'Propert%s "%s" should not have changed type',
-                    $plural ? 'ies' : 'y',
-                    implode('" & "', $typeErrors)
-                ));
-            }
-
-            // Test Invocation
-            $params = [
-                'visitor' => \Closure::fromCallable(fn (array $line) => $line[2]),
-                'key' => 'value',
-                'config' => ['basePath' => '/some/base/path']
-            ];
-
-            $obj = new $visitor->classname(...$params);
-
-            // Need to pull updates so can use this maybe
-            //collect($class->getProperties())->map()->flatten()->filter(); or flatMap->filter
-
-            $actual = array_merge([], ...array_map(function ($property) use ($obj) {
-                $property->setAccessible(true);
-                $name = $property->getName();
-                $value = $property->isInitialized($obj) ? $property->getValue($obj) : null;
-
-                return [$name => $value];
-            }, $class->getProperties()));
-
-            $dataFailures = array_filter([
-                'visitor' => $params['visitor'] !== $actual['visitor'],
-                'key' => $params['key'] !== $actual['key'],
-                'basePath' => $params['config']['basePath'] !== $actual['basePath'],
-            ]);
-
-            if (count($dataFailures)) {
-                $plural = count($dataFailures) > 1;
-                return Failure::fromNameAndReason($this->getName(), sprintf(
-                    'Data not correctly set on propert%s "%s"',
-                    $plural ? 'ies' : 'y',
-                    implode('" & "', array_keys($dataFailures))
-                ));
-            }
-
-            return new Success($this->getName());
-        })();
+        return $visitor->classname;
     }
 }
